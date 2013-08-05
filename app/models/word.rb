16096319dabc280ecd8748f21a741e1f21ace6cf
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-05 16:57:11 -0500

    started anagram mnethod

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,6 +1,15 @@
 class Word < ActiveRecord::Base
   # Remember to create a migration!
-  def sort_word
-    self.split(" ").sort
+  def sort_word(word)
+    word.split("").sort.join
+  end
+
+  def self.anagrams(word)
+    @answer = []
+    Words.all.each do |i|
+      if i[:sorted_letters] == sort_word(word)
+        @answer << i[:word]
+      end
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 dictionary = File.open('/usr/share/dict/words')
 dictionary.each do |word|
-Word.create!(word: word, sorted_letters: word.split('').sort.join)
+Word.create!(word: word.chomp.downcase, sorted_letters: word.chomp.downcase.split('').sort.join)
 end
